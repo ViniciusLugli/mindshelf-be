89c48dd3f5f5547316945217e1554fe1c40cac40
Vinícius <viniciusflugli@gmail.com>
2026-03-15 14:09:16 -0300

    feat: add user routes and handler integration in main application

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/ViniciusLugli/mindshelf/internal/handlers"
 	"github.com/ViniciusLugli/mindshelf/internal/repositories"
+	"github.com/ViniciusLugli/mindshelf/internal/services"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
@@ -25,7 +27,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	userRepository := repositories.NewUserRepository(db)
+	userService := services.NewUserService(userRepository)
+	userHandler := handlers.NewUserHandler(userService)
+
 	router := gin.Default()
+
+	{
+		userRoute := router.Group("/user")
+		userRoute.GET("/", userHandler.GetUser)
+		userRoute.GET("/all", userHandler.GetAllUsers)
+		userRoute.POST("/create", userHandler.Create)
+		userRoute.PATCH("/update", userHandler.Update)
+		userRoute.DELETE("/delete", userHandler.Delete)
+	}
 
 	router.Run()
 }
