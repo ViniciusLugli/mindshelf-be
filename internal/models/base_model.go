5d4c2b4cefc15d7837f37b7f2be3a5d281606d84
Vinícius <viniciusflugli@gmail.com>
2026-03-21 01:36:40 -0300

    refactor: replace Deleted bool with gorm.DeletedAt in BaseModel

--- internal/models/base_model.go
+++ internal/models/base_model.go
@@ -11,8 +11,7 @@ type BaseModel struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Deleted   bool
-	DeletedAt *time.Time `gorm:"index"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 func (b *BaseModel) BeforeCreate(tx *gorm.DB) error {
